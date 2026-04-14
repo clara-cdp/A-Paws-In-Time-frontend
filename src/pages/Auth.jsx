9d73c12e4b0e680bcecefc1376d75f24c4f6310d
clara-cdp <clarianne.cdp@gmail.com>
2026-04-14 11:11:43 +0200

    basic page layout

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,13 @@
+export default function Auth() {
+  return (
+    <div>
+      <h1>Login / Register</h1>
+      <p>Endpoints mapping:</p>
+      <ul>
+        <li>POST /auth/login</li>
+        <li>POST /auth/register</li>
+        <li>(logout)</li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Endpoints mapping:</p>
+      <ul>
+        <li>GET /me</li>
+        <li>PUT /me</li>
+        <li>DELETE /me</li>
+      </ul>
+    </div>
+  )
+}
