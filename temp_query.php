5e09568e68661a869a9a445313ce57abff1074ce
clara-cdp <clarianne.cdp@gmail.com>
2026-04-23 12:38:56 +0200

    prepared files to docker

--- temp_query.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('sqlite:C:/Users/Clara/Desktop/IT ACADEMY/APIT-API/database/database.sqlite');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$games = $db->query("SELECT id, room_id, progress FROM games ORDER BY id DESC LIMIT 5")->fetchAll(PDO::FETCH_ASSOC);
-print(json_encode($games, JSON_PRETTY_PRINT));
